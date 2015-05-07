8f23168ddbef57a6366e87decee4767729597b43
albertosouza <alberto.souza.99@gmail.com>
2015-05-07 17:55:18 -0300

    update grunt copy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -320,7 +320,8 @@ module.exports = function(grunt) {
                         '*.{ico,txt}',
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
-                        'styles/fonts/*'
+                        'styles/fonts/*',
+                        'styles/i/*'
                     ]
                 }]
             }
